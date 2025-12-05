ab7a6a9e49f0ebb438d9299e1166e3d9711ad5be
Giovanni Faldani <faldanig@yahoo.it>
2025-12-05 19:25:43 +0100

    Revert "Remove UI Code corruption fix maybe"
    This reverts commit ccf3fc95f91ad7b23531283235e53374b41396d7.

--- Source/TheGauntlet/_Game/CPP_PlayerController.h
+++ Source/TheGauntlet/_Game/CPP_PlayerController.h
@@ -18,17 +18,17 @@ class THEGAUNTLET_API ACPP_PlayerController : public APlayerController
 
 protected:
 	// The class of the HUD to spawn (Assign this in BP_GauntletPlayerController)
-	//UPROPERTY(EditDefaultsOnly, Category = "UI")
-	//TSubclassOf<UHUDWidget> HUDWidgetClass;
+	UPROPERTY(EditDefaultsOnly, Category = "UI")
+	TSubclassOf<UHUDWidget> HUDWidgetClass;
 
 	// The actual instance of the HUD
-	//UPROPERTY()
-	//TObjectPtr<UHUDWidget> HUDWidgetInstance;
+	UPROPERTY()
+	TObjectPtr<UHUDWidget> HUDWidgetInstance;
 
 public:
 	// Print message to screen from anywhere
-	//UFUNCTION(BlueprintCallable, Category = "UI")
-	//void PublishUIMessage(FString Message);
+	UFUNCTION(BlueprintCallable, Category = "UI")
+	void PublishUIMessage(FString Message);
 
 protected:
 	// Called when the game starts or when spawned
@@ -40,10 +40,10 @@ protected:
 	void BindToPawnDelegates(APawn* InPawn);
 
 	// Callback when the Pawn broadcasts a health change
-	//UFUNCTION()
-	//void HandlePawnHealthChanged(float NewHealth, float MaxHealth);
+	UFUNCTION()
+	void HandlePawnHealthChanged(float NewHealth, float MaxHealth);
 
-	//// Callback when the Pawn broadcasts interactable actor found
-	//UFUNCTION()
-	//void HandleInteractionMessage(bool Found);
+	// Callback when the Pawn broadcasts interactable actor found
+	UFUNCTION()
+	void HandleInteractionMessage(bool Found);
 };
